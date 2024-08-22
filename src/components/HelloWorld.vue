89e326b62073c6cb2f6208f01d43a4f6aa5f8c0a
Lilwen4399 <202200300050@mail.sdu.edu.cn>
2024-08-22 20:46:43 +0800

    初步构建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="loginCard">
+    <ElInput class=loginInput v-model="account" placeholder="请输入账号" />
+    <ElInput class=loginInput type=password v-model="password" placeholder="请输入密码" />
+    <ElButton class=loginButton type="primary" @click="login()">登录</ElButton>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { ref } from 'vue';
+import { ElInput,ElButton } from 'element-plus';
+const account = ref('');
+const password = ref('');
+
+export default {
+  data(){
+    return {
+      account,
+      password
+    }
+  },
+  methods:{
+    login(){
+      console.log(this.account,this.password)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loginButton{
+  width: 100%;
+}
+.loginInput{
+  height: 50px;
+  padding-bottom: 10px;
+}
+</style>
